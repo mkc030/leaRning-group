1ceb972d525311ab8eeabc38ac60a551cdc0313a
federicoandrade <federicoandrade@gmail.com>
2020-06-28 15:17:03 -0700

    Wrote the script to be converted in function

--- capstone_projects/mini_capstone/Fede_Capstone/add_column_start.R
+++ capstone_projects/mini_capstone/Fede_Capstone/add_column_start.R
@@ -10,21 +10,30 @@ library(tidyverse)  #Contains purrr
 library(lubridate)
 
 # Load and examine data
-load("capstone_projects/mini_capstone/edata_function_writing.Rdata")
+load("capstone_projects/mini_capstone/Fede_Capstone/edata_function_writing.Rdata")
 head(edata)
 tail(edata)
 glimpse(edata)
 
 ### Goal 1: Turn 'date_col' into a column of 'Date' class  ------------------------------------------------------------------------------------------------------------
 
-
-
+edata$date_col <- edata$date_col %>% as_date(tz = NULL, format = NULL)
+sum(is.na(edata$date_col))
 
 
 ### Goal 2: Add a column to edata for mon_yr using a function: WITHOUT purrr  -----------------------------------------------------------------------------------------------
 
-add.monyr <- function(ARGUMENTorARGUMENTS){    
-                BODYofFUNCTION
+### Trying the potential script
+edataPrueba <- edata %>% 
+               select(date_col) %>% 
+               mutate(mon = month(date_col, label = TRUE, abbr = TRUE), yr = year(date_col)) %>% 
+               unite("monyr", mon, yr, sep = "-") %>% 
+               arrange(date_col)
+          
+
+
+add.monyr <- function(){    
+                BODY
               }
 
 
